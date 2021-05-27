92da6c15c20f7c5ad5394c8907a199c49d046d08
saba-can00 <kangaroo.and.kaffee@gmail.com>
2021-05-27 23:05:15 +0900

    move add.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { AppHeader } from "./AppHeader";
+
+// function AppHeader(props) {
+//   return <header>TDEE 計算ツール</header>;
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader></AppHeader>
+    </div>
+  );
+}
+
+export default App;
