9d0151540b941b78372284d1a0657e89fd796ba6
saba-can00 <kangaroo.and.kaffee@gmail.com>
2021-05-31 18:43:18 +0900

    modify radio buttons style to one with original color

--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 const RadioButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 200px;
 `;
 
 const SelectionContainer = styled.div`
@@ -32,6 +31,34 @@ const SelectionLabel = styled.label`
   text-align: left;
   font-size: 24px;
   line-height: 36px;
+  &:before {
+    content: "";
+    border-radius: 50%;
+    border: 2px solid #6ed085;
+    width: 20px;
+    height: 20px;
+    display: inline-flex;
+    margin-right: 4px;
+    vertical-align: middle;
+  }
+`;
+
+const ButtonInput = styled.input`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border-radius: 50%;
+  position: relative;
+  left: 17px;
+  width: 10px;
+  height: 10px;
+  margin: 0;
+  background: #6ed085;
+  opacity: 0;
+  transition: opacity 0.1s ease;
+  &:checked {
+    opacity: 1;
+  }
 `;
 
 export class RadioButton extends React.Component {
@@ -41,13 +68,19 @@ export class RadioButton extends React.Component {
         <RadioButtonGroupLabel>{this.props.label}</RadioButtonGroupLabel>
         <SelectionContainer>
           <Selection>
-            <input type="radio" id="men" name={this.props.name} value="men" />
+            <ButtonInput
+              checked
+              type="radio"
+              id="men"
+              name={this.props.name}
+              value="men"
+            />
             <SelectionLabel htmlFor="men">
               {this.props.selection}
             </SelectionLabel>
           </Selection>
           <Selection>
-            <input
+            <ButtonInput
               type="radio"
               id="women"
               name={this.props.name}
