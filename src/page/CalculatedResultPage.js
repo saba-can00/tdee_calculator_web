85c8381ee38363ed784590ab1576cfd7f473875e
saba-can00 <kangaroo.and.kaffee@gmail.com>
2021-06-05 21:55:47 +0900

    create CalculatedResultPage

---
+++ src/page/CalculatedResultPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+export class CalculatedResultPage extends React.Component {
+  render() {
+    <Form></Form>;
+  }
+}
+
+const Form = styled.main`
+  display: flex;
+  margin: 24px auto;
+`;
